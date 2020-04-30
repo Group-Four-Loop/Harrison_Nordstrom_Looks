49fb47e44f6ed5709138e9a4ae0f31665453eaef
phileasthefogg <54925839+phileasthefogg@users.noreply.github.com>
2020-04-30 16:32:51 -0700

    Revert "Add .circleci/config.yml"

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-//setup how jest runs
-  //setupFiles takes an array of files that need to be run before each block of tests
-module.exports  = {
-  setupFiles: ['<rootDir>/react-client/test/setup.js']
-}
--- react-client/test/setup.js
+++
@@ -1,6 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-//call Enzyme's configure method and pass an object
-Enzyme.configure({adapter: new Adapter() });
-//our object has a single property 'adapter' to which we will assign a new enzyme-react-adapter instance.
