fd0d4865d3e620102de7bd4a1bdecf6a47d87ab5
thehlau <thehlau@gmail.com>
2020-05-02 13:49:21 -0700

    added route to serve list of all products from db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const db = require('../db/connection.js');
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,16 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  db.getAllItems((err, result) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.send(result.rows);
+    }
+  });
+});
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.listen(port, () => {
