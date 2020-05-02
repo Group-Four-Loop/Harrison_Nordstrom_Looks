2732184978c284609b4a66a40685f6007fb84ad7
thehlau <thehlau@gmail.com>
2020-05-01 17:15:14 -0700

    stupid export thing

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -28,5 +28,5 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
 export default App;
