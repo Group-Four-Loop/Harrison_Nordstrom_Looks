b8c46c26b59ae9d7684236c898998448cc135f43
thehlau <thehlau@gmail.com>
2020-05-03 13:35:16 -0700

    commit before merging up branch

---
+++ db/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: 'localhost',
+  user: 'harrison',
+  database: 'nordstrom',
+  port: 5432
+};
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -22,11 +22,10 @@ class App extends React.Component {
       <div>HELLO WORLD
         <p onClick= {() => { this.handleClick(); }}>Count {this.state.count}</p>
         <Look />
-        <image src="https://fec-fourloop-looks.s3-us-west-1.amazonaws.com/Footwear/bfa5f26f-acbe-4352-a285-e9ddb3b7d558.jpeg"/>
       </div>
     );
   }
 }
 
-// ReactDOM.render(<App />, document.getElementById('app'));
-export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
+//export default App;
