1349867e0c1229067b1fc35daa77726c9b899b3f
thehlau <thehlau@gmail.com>
2020-05-04 15:48:06 -0700

    ugly as hell but slider is working properly~~~

--- react-client/src/components/Bottoms.jsx
+++ react-client/src/components/Bottoms.jsx
@@ -3,15 +3,29 @@ import styled from 'styled-components';
 import Picture from './Picture.jsx';
 
 const Row = styled.div`
+  position: relative;
+  width:100%;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
   display:flex;
-  flex-direction:row;
-  justify-content: space-around;
-  min-height: 200px;
-  order: 0
+  align-items:center;
+  order: 0;
+  overflow:hidden;
 `;
-const Frame = styled.div`
-  background-color: black;
+const RightArrow = styled.div`
+  width:10%;
+  position:absolute;
+  height: 80%;
+  right:0;
 `;
+const LeftArrow = styled.div`
+position:absolute;
+  width:10%;
+  height: 80%;
+  left:0;
+`;
+
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -20,6 +34,8 @@ class Carousel extends React.Component {
       items: props.items,
       viewport: 0
     };
+    this.shiftLeft = this.shiftLeft.bind(this);
+    this.shiftRight = this.shiftRight.bind(this);
   }
   componentDidUpdate(prevProps) {
     // Typical usage (don't forget to compare props):
@@ -27,14 +43,30 @@ class Carousel extends React.Component {
       this.setState({items: this.props.items});
     }
   }
+
+  shiftLeft() {
+    console.log('shiftleft')
+    let currentView = this.state.viewport;
+    this.setState({viewport: currentView - 100}, ()=>console.log(this.state.viewport))
+  }
+  shiftRight() {
+    console.log('shiftright')
+    let currentView = this.state.viewport;
+    this.setState({viewport: currentView + 100}, ()=>console.log(this.state.viewport))
+  }
+
   render() {
+    const vp = this.state.viewport;
+    console.log('vp', vp)
     return (
-      <Row key={this.state.items[0].id}>
+      <Row key={this.state.items[0].id} >
+        <LeftArrow onClick={this.shiftLeft}>LEFT</LeftArrow>
         {this.state.items.map((item) => {
           return (
-            <Picture key={item.imgurl} product={item} />
+            <Picture key={item.imgurl} product={item} position={{transform: `translateX(${vp}%)`}}/>
           );
         })}
+        <RightArrow onClick={this.shiftRight}>RIGHT</RightArrow>
       </Row>
     );
   }
--- react-client/src/components/Picture.jsx
+++ react-client/src/components/Picture.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Product = styled.img`
-  max-height: 340px;
-  max-width: 143px;
+const Product = styled.div`
+  margin: 1rem;
+  transition: .5s;
+  overflow:hidden;
 `;
 
 const Pictures = (props) => {
   console.log(props.product.type, props);
   return (
-    <Product src={`${props.product.imgurl}`} key={props.product.id}>
+    <Product key={props.product.id} style={props.position}>
+      <img src={`${props.product.imgurl}`}></img>
     </Product>
   );
 };
