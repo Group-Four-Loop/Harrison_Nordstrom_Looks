5a60b7f735a88da720377ada8de31e1702a3e95a
phileasthefogg <54925839+phileasthefogg@users.noreply.github.com>
2020-10-06 21:10:52 -0700

    Update Looks.js
    Removed extra comments

--- react-client/src/models/Looks.js
+++ react-client/src/models/Looks.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const looks = {
   getLooksByProductId: (product) => axios.get('/api/looks', {params: {productId: product}}).then(({data}) => data ),
-  // getLooksByProductId: (product) => axios.get('/api', {params: {lookId: product}}).then(({data}) => data ),
 };
 
 export default looks;
